35678fc6f8f50934ae6b261947cd844757ccc240
hdju <kiribati1234@gmail.com>
2026-01-24 11:57:47 +0900

    파일명 변경

--- backend/app/api/v1/__init__.py
+++ backend/app/api/v1/__init__.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter
-from . import auth
+from . import auth_api
 
 router = APIRouter()
 
-# auth 라우터 포함
-router.include_router(auth.router)
+# auth_api 라우터 포함
+router.include_router(auth_api.router)
